ede7ac59110055331872751b865a4f5d0c5c90c8
Ulrik Damm <ulrikdamm@me.com>
2015-10-26 10:13:25 +0100

    Initial Commit

---
+++ Sqlable/Sqlable.h
@@ -0,0 +1,19 @@
+//
+//  Sqlable.h
+//  Sqlable
+//
+//  Created by Ulrik Damm on 26/10/2015.
+//  Copyright © 2015 Ufd.dk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sqlable.
+FOUNDATION_EXPORT double SqlableVersionNumber;
+
+//! Project version string for Sqlable.
+FOUNDATION_EXPORT const unsigned char SqlableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sqlable/PublicHeader.h>
+
+
---
+++ SqlableTests/SqlableTests.swift
@@ -0,0 +1,36 @@
+//
+//  SqlableTests.swift
+//  SqlableTests
+//
+//  Created by Ulrik Damm on 26/10/2015.
+//  Copyright © 2015 Ufd.dk. All rights reserved.
+//
+
+import XCTest
+@testable import Sqlable
+
+class SqlableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
